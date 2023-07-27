d6432f5aeb09b58948b1b93697000e0977dc8a7b
Daniel <danieldetrabalho@hotmail.com>
2023-07-27 20:56:33 -0300

    Melhora no alinhamento e no javascript

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 const form = document.querySelector("form")
+// Criando um paragrafo
+const resultado = document.createElement("div")
+//Colocando os novos elementos como filho do formulario
+document.body.appendChild(resultado)
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
     const poke = form.poke.value
+    const regex = /^[A-Za-z0-9\s]+$/
+    if (poke.trim() === "" || !poke.match(regex)) {
+        alert("Preencha um nome de um Pokemon ou um número de 0 até 1010")
+        return
+    }
     //Usando a api do pokeapi com o framework do ajax
     $.ajax({
         url: `https://pokeapi.co/api/v2/pokemon/${poke}`, success: function (result) {
-            // Criando um paragrafo
-            const resultado = document.createElement("div")
-            const nome = document.createElement("p")
-            const peso = document.createElement("p")
-            const altura = document.createElement("p")
-            const imagem = document.createElement("img")
-            // //Colocando os novos elementos como filho do formulario
-            form.append(nome)
-            form.append(peso)
-            form.append(altura)
-            form.append(imagem)
-            document.body.append(resultado)
-            resultado.append(imagem)
-           // mostrando a mensagem dinamicamente
-            nome.innerHTML = (`Nome: ${result.name}`)
-            peso.innerHTML = (`Peso: ${result.weight}`)
-            altura.innerHTML = (`Altura: ${result.height}`)
-            imagem.style.width = "300px"
-            imagem.style.height = "300px"
-            imagem.src = (`${result.sprites.front_default}`)
-
+            // mostrando a mensagem dinamicamente
+            const conteudo = `
+            <p class="Personalizar"><strong>Nome:</strong> ${result.name}</p>
+            <p class="Personalizar"><strong>Peso:</strong> ${result.weight}\&nbsp;kg</p>
+            <p class="Personalizar"><strong>Altura:</strong> ${result.height}&nbsp;m</p>
+            <img src="${result.sprites.front_default}" alt="${result.name}" />`
+            resultado.style.display = "block"
+            resultado.innerHTML = conteudo
         }
     })
 })
