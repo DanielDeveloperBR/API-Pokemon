4de5509ae5eb75ae4c037542ac01e8dc0dd41ed3
Daniel <danieldetrabalho@hotmail.com>
2023-07-29 01:15:28 -0300

    Mudei as cores, melhorei as validações e coloquei uma animação na imagem

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const form = document.querySelector("form")
 // Criando um paragrafo
 const resultado = document.createElement("div")
+const principal = document.querySelector("section")
+principal.appendChild(resultado)
 //Colocando os novos elementos como filho do formulario
-document.body.appendChild(resultado)
+
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    const poke = form.poke.value
-    const regex = /^[A-Za-z0-9\s]+$/
-    if (poke.trim() === "" || !poke.match(regex)) {
-        alert("Preencha um nome de um Pokemon ou um número de 0 até 1010")
+    const poke = form.poke.value.trim()
+    if (poke === "" || poke <= 0) {
+        alert("Preencha um nome de um Pokemon ou um número de 1 até 1010")
         return
     }
     //Usando a api do pokeapi com o framework do ajax
@@ -19,9 +20,13 @@ form.addEventListener("submit", (evento) => {
             <p class="Personalizar"><strong>Nome:</strong> ${result.name}</p>
             <p class="Personalizar"><strong>Peso:</strong> ${result.weight}\&nbsp;kg</p>
             <p class="Personalizar"><strong>Altura:</strong> ${result.height}&nbsp;m</p>
-            <img src="${result.sprites.front_default}" alt="${result.name}" />`
+            <img src="${result.sprites.front_default}" alt="${result.name}" title="${result.name}" />`
             resultado.style.display = "block"
             resultado.innerHTML = conteudo
-        }
+        },
+        error: function(){
+            resultado.innerHTML = ""
+            alert("Pokémon não encontrado")
+        },
     })
 })
